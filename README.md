36c42ba59f99551f84711892cee49322553dc9d7
Luotingwei <luotingweiltw@gmail.com>
2021-09-06 18:53:37 +0800

    数据库建立

--- README.md
+++ README.md
@@ -14,7 +14,12 @@ https://docs.github.com/en GitHub API文档
 
 https://square.github.io/okhttp Okhttp官网
 
-  
+https://mybatis.org/spring-boot-starter/mybatis-spring-boot-autoconfigure/ mybatis集成spring boot 说明文档
+
+https://www.h2database.com/html/main.html h2数据库官网
+
+https://docs.spring.io/spring-boot/docs/2.5.3/reference/htmlsingle/#features.sql.datasource.embedded Spring Boot 文档
+
 ## 使用工具
 
 https://git-scm.com/download git下载地址
--- src/main/java/com/ngb/community/contorller/AuthorizeController.java
+++ src/main/java/com/ngb/community/contorller/AuthorizeController.java
@@ -2,6 +2,8 @@ package com.ngb.community.contorller;
 
 import com.ngb.community.dto.AccessTokenDto;
 import com.ngb.community.dto.GithubUser;
+import com.ngb.community.mapper.UserMapper;
+import com.ngb.community.model.User;
 import com.ngb.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,15 +12,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
+
+    @Autowired
+    private UserMapper userMapper;
+
     @Value("${github.client.id}")
     private String clientId;
+
     @Value("${github.client.secret}")
     private String clientSecret;
+
     @Value("${github.redirect.uri}")
     private String redirectUri;
 
@@ -32,10 +41,17 @@ public class AuthorizeController {
         accessTokenDto.setClient_id(clientId);
         accessTokenDto.setClient_secret(clientSecret);
         String access_token = githubProvider.getAccessToken(accessTokenDto);
-        GithubUser user = githubProvider.getUser(access_token);
-        if (null != user) {
+        GithubUser githubUser = githubProvider.getUser(access_token);
+        if (null != githubUser) {
             //登录成功，构造cookie和session
-            request.getSession().setAttribute("user", user);
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccount_id(String.valueOf(githubUser.getId()));
+            user.setGmt_create(System.currentTimeMillis());
+            user.setGmt_modified(user.getGmt_create());
+            userMapper.insert(user);
+            request.getSession().setAttribute("githubUser", githubUser);
             return "redirect:/";
         } else {
             //登录失败，重新登录
